04c060c21a8d50130548c20d1fc95d52ed2a654f
rezajafarpour <rezajafarpour222@gmail.com>
2026-08-09 14:46:12 +0330

    Add: encrypt,decrypt

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,1 @@
-fn main() {
-    let salt = encryption::encryption::generate_salt();
-
-    println!("salt: {:02x?}", salt);
-}
+fn main() {}
--- encryption/src/encryption.rs
+++ encryption/src/encryption.rs
@@ -1,3 +1,4 @@
+use aes_gcm::{Aes256Gcm, Key, KeyInit, Nonce, aead::Aead};
 use argon2::{Algorithm, Argon2, Params, Version};
 use rand::Rng;
 pub fn derive_key(password: &[u8], salt: &[u8]) -> [u8; 32] {
@@ -21,4 +22,33 @@ pub fn generate_salt() -> [u8; 16] {
     let mut salt = [0u8; 16];
     rand::rng().fill_bytes(&mut salt);
     salt
+}
+
+pub fn encrypt(
+    key_bytes: &[u8; 32],
+    plaintext: &[u8],
+) -> Result<(Vec<u8>, [u8; 12]), aes_gcm::Error> {
+    let key =
+        Key::<Aes256Gcm>::try_from(key_bytes.as_slice()).expect("key must be exactly 32 bytes");
+    let cipher = Aes256Gcm::new(&key);
+
+    let mut nonce_bytes = [0u8; 12];
+    rand::rng().fill_bytes(&mut nonce_bytes);
+    let nonce = Nonce::try_from(nonce_bytes.as_slice()).expect("nonce must be exactly 12 bytes");
+
+    let ciphertext = cipher.encrypt(&nonce, plaintext)?;
+    Ok((ciphertext, nonce_bytes))
+}
+
+pub fn decrypt(
+    key_bytes: &[u8; 32],
+    nonce_bytes: &[u8; 12],
+    ciphertext: &[u8],
+) -> Result<Vec<u8>, aes_gcm::Error> {
+    let key =
+        Key::<Aes256Gcm>::try_from(key_bytes.as_slice()).expect("key must be exactly 32 bytes");
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Nonce::try_from(nonce_bytes.as_slice()).expect("nonce must be exactly 12 bytes");
+
+    cipher.decrypt(&nonce, ciphertext)
 }
